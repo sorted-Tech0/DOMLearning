d3147da81b43bba80d13a60daebfeaa0d3abf904
sorted-Tech0 <shivanesforcode@gmail.com>
2021-08-06 03:33:07 +0530

    nextElementSibling + previousElementSibling

---
+++ 09 nextElementSibling-previousElementSibling/app.js
@@ -0,0 +1,9 @@
+// previousElementSibling  && nextElementSibling
+
+const first = document.querySelector('.first');
+first.nextElementSibling.style.color = "red";
+
+const last = document.querySelector('#last');
+last.previousElementSibling.style.color = "blue";
+
+// TIP: Here you don't have to really care about the white text
