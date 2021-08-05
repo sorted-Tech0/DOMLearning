8b3b5a124a958368b4e770623c6735e4ba2adb6f
sorted-Tech0 <shivanesforcode@gmail.com>
2021-08-06 01:55:45 +0530

    understanding getElementById() method

---
+++ 02 getElementById/app.js
@@ -0,0 +1,18 @@
+// select the element or group of element the we want
+// decide the effect we want to apply to the selection
+
+// getElementById('element)
+
+const h1 = document.getElementById('title');
+h1.style.color = 'red';      //     'id'
+
+// accessing and updating directly
+// document.getElementById('btn').style.backgroundColor = 'blue';
+
+// document.getElementById('btn').style.color = "white";
+
+// or can be s=done like this
+
+const btn = document.getElementById('btn');
+btn.style.backgroundColor = "blue";    // changing background color
+btn.style.color = "white";     // changing text color
