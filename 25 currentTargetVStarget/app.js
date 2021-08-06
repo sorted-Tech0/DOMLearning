5393782b9ce7cd3923f99aacb4b8ce55b8b0952a
sorted-Tech0 <shivanesforcode@gmail.com>
2021-08-06 22:33:29 +0530

    currentTarget Vs target

---
+++ 25 currentTargetVStarget/app.js
@@ -0,0 +1,14 @@
+// currentTarget -> always refers to the element to which the event handler has been attached to
+// target -> identifies the element on which the event occured
+
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        // console.log(e.currentTarget)
+        // e.currentTarget.style.color = 'green';
+
+        console.log(e.target);
+        e.target.style.color = 'green';     // will reflect into green on which section you clicked on
+    })
+});
