0991dabda218e1d9fe22250b9e8eeedbfd17dc45
sorted-Tech0 <shivanesforcode@gmail.com>
2021-08-06 18:10:52 +0530

    addEventListener()

---
+++ 20 addEventListener/app.js
@@ -0,0 +1,11 @@
+// select element
+// addEventListener()
+// what event, what to do
+
+const btn = document.querySelector('.btn');
+const heading = document.querySelector('h2');
+
+btn.addEventListener('click', function(){
+  
+    heading.classList.add('red');
+})
