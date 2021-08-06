9ce0a6d8ac52e8cd75dac09ef5d9c1855a03f1f0
sorted-Tech0 <shivanesforcode@gmail.com>
2021-08-06 17:52:14 +0530

    change CSS using JS

---
+++ 19 changeCssStyle/app.js
@@ -0,0 +1,12 @@
+// CSS
+
+const random = document.querySelector('.random');
+
+// random.style.backgroundColor = "blue";
+// random.style.Color = "white";
+// random.style.fontSize = "3rem";
+// random.style.textTransform = "capitalize";
+
+// or instead we can do like
+
+random.classList.add('title')    // same as above
